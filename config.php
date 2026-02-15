bacbe93393909a4f5add935fe4b1d9fa2f3e9819
om pradip patil <omspradippatil@gmail.com>
2026-02-15 21:04:33 +0530

    update

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 // Database Configuration
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'om');
+define('DB_HOST', 'sql308.infinityfree.com');
+define('DB_USER', 'if0_41165672');
+define('DB_PASS', 'om27031980');
+define('DB_NAME', 'if0_41165672_om');
 define('DB_PORT', 3306);
 
 // Website Configuration
-define('SITE_URL', 'http://localhost/IndianNGOWebsite/');
+define('SITE_URL', 'https://indianngowebsite.ct.ws/');
 define('SITE_NAME', 'Indian NGO Management System');
 
 // Security
